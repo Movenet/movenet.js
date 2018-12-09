c5fd91bc9f0e0e4e2910bc49033f4829635a718b
James-Livesey <jamesliveg@gmail.com>
2018-12-09 17:44:44 +0000

    Add getNearbyStops

--- src/js/movenet.js
+++ src/js/movenet.js
@@ -1,37 +1,78 @@
 var movenet = (function() {
+    var internalConfig = {
+        apiKey: "AIzaSyAuFypseM3j0A6OcnX3H1MOnv3vYboDBQs",
+        authDomain: "movenet-apis.firebaseapp.com",
+        databaseURL: "https://movenet-apis.firebaseio.com",
+        projectId: "movenet-apis",
+        storageBucket: "movenet-apis.appspot.com",
+        messagingSenderId: "864507894049"
+    };
+
+    movenetinternal.initializeApp(internalConfig);
+
+    function constructAPIEnding(name, callback) {
+        movenetinternal.database().ref("apilist/" + name).once("value").then(function(snapshot) {
+            if (snapshot.val()["-type"] == "query") {
+                var stringBuild = "";
+
+                for (var key in snapshot.val()) {
+                    if (snapshot.val().hasOwnProperty(key) && key != "-type") {
+                        stringBuild += "&" + key + "=" + snapshot.val()[key];
+                    }
+                }
+
+                callback(stringBuild);
+            }
+        });
+    }
+
     return {
         properties: {
             apiKey: null
         },
 
+/*
+ * @function movenet.init
+ * @params [{"name": "config", "struct": {"apiKey": "string"}}]
+ * @info Initialises movenet.js keys.
+ */
+
         init: function(config) {
             this.properties = config;
         },
 
-        transport: {
-            buses: {
+/*
+ * @function movenet.getNearbyStops
+ * @params [{"name": "location", "struct": {"latitude": "number", "longitude": "number"}}, {"name": "callback", "struct": "function"}]
+ * @return [{"type": "bus/train/metro/tram/ferry/plane/other", "name": "data", "location": {"latitude": "number", "longitude": "number", "accuracy": "number", "hrname": "string"}}]
+ * @info Gets data for stop or station.
+ */
 
-            },
+        getNearbyStops: function(location, callback) {
+            constructAPIEnding("transportapi", function(ending) {
+                $.getJSON("http://transportapi.com/v3/uk/places.json?lat=" + location.latitude + "&lon=" + location.longitude + "&type=bus_stop" + ending, function(json) {
+                    var build = [];    
+                    var typeTranslator = {
+                        "bus_stop": "bus",
+                        "train_station": "train"
+                    };
+                
+                    for (var i = 0; i < json.member.length; i++) {
+                        build.push({
+                            "type": typeTranslator[json.member[i].type],
+                            "name": json.member[i].name,
+                            "location": {
+                                "latitude": json.member[i].latitude,
+                                "longitude": json.member[i].longitude,
+                                "accuracy": json.member[i].accuracy,
+                                "hrname": json.member[i].description
+                            }
+                        });
+                    }
 
-            trains: {
-
-            },
-
-            metro: {
-
-            },
-
-            trams: {
-
-            },
-
-            planes: {
-
-            },
-
-            other: {
-
-            }
+                    callback(build);
+                });
+            });
         }
     };
 })();
