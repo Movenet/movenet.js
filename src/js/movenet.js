b6417b68fdebfc923ab3aa1d61f62f1617208225
James-Livesey <jamesliveg@gmail.com>
2018-12-09 18:25:18 +0000

    Add filter for transportation methods

--- src/js/movenet.js
+++ src/js/movenet.js
@@ -43,31 +43,53 @@ var movenet = (function() {
 
 /*
  * @function movenet.getNearbyStops
- * @params [{"name": "location", "struct": {"latitude": "number", "longitude": "number"}}, {"name": "callback", "struct": "function"}]
+ * @params [{"name": "location", "struct": {"latitude": "number", "longitude": "number"}}, {"name": "filter", "struct": {"type": ["postcode/bus/train/metro/tram/ferry/plane/other"]}}, {"name": "callback", "struct": "function"}]
  * @return [{"type": "bus/train/metro/tram/ferry/plane/other", "name": "data", "location": {"latitude": "number", "longitude": "number", "accuracy": "number", "hrname": "string"}}]
  * @info Gets nearby stops around location.
  */
 
-        getNearbyStops: function(location, callback) {
+        getNearbyStops: function(location, filter, callback) {
             constructAPIEnding("transportapi", function(ending) {
-                $.getJSON("https://transportapi.com/v3/uk/places.json?lat=" + location.latitude + "&lon=" + location.longitude + "&type=bus_stop" + ending, function(json) {
+                $.getJSON("https://transportapi.com/v3/uk/places.json?lat=" + location.latitude + "&lon=" + location.longitude + ending, function(json) {
                     var build = [];    
                     var typeTranslator = {
+                        "postcode": "postcode",
                         "bus_stop": "bus",
+                        "tram_stop": "tram",
+                        "tube_station": "metro",
                         "train_station": "train"
                     };
+
+                    if (!filter) {
+                        filter = {};
+                    }
                 
                     for (var i = 0; i < json.member.length; i++) {
-                        build.push({
-                            "type": typeTranslator[json.member[i].type],
-                            "name": json.member[i].name,
-                            "location": {
-                                "latitude": json.member[i].latitude,
-                                "longitude": json.member[i].longitude,
-                                "accuracy": json.member[i].accuracy,
-                                "hrname": json.member[i].description
+                        if (filter.type == [] || filter.type == undefined || filter.type == null) {
+                            build.push({
+                                "type": typeTranslator[json.member[i].type],
+                                "name": json.member[i].name,
+                                "location": {
+                                    "latitude": json.member[i].latitude,
+                                    "longitude": json.member[i].longitude,
+                                    "accuracy": json.member[i].accuracy,
+                                    "hrname": json.member[i].description
+                                }
+                            });
+                        } else {
+                            if (filter.type.includes(typeTranslator[json.member[i].type])) {
+                                build.push({
+                                    "type": typeTranslator[json.member[i].type],
+                                    "name": json.member[i].name,
+                                    "location": {
+                                        "latitude": json.member[i].latitude,
+                                        "longitude": json.member[i].longitude,
+                                        "accuracy": json.member[i].accuracy,
+                                        "hrname": json.member[i].description
+                                    }
+                                });
                             }
-                        });
+                        }
                     }
 
                     callback(build);
